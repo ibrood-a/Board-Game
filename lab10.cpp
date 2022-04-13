527e3049906f77536810dd26ce3db0e0b0153ba6
iBlooze <iblooze2@gmail.com>
2022-04-13 15:06:02 -0500

    Add files via upload
    changed it to deque so that we dont have to use stack then reverse it

--- lab10.cpp
+++ lab10.cpp
@@ -3,55 +3,94 @@
 
 #include <iostream>
 #include <string>
-#include <vector>
+#include <deque>
 #include <stack>
 
 using namespace std;
 
+struct tile {
+    int curCol;
+    int curRow;
+    int curValue;
+    
+    tile(int col, int row, int val) {
+        curCol = col;
+        curRow = row;
+        curValue = val;
+    }
+};
+
 // print out the vector as a 3x3 grid
-void print( vector<int>& vec );
+void print( deque<tile>& vec );
 
 // function used to convert a single number into a vector of size 9 with single digit values
-void addElements( vector<int>& test, int puzzle );
+void addElements( deque<tile>& test, int puzzle );
 
 int main( int argc, char** argv ) {
-	vector<int> puzzleVec; // vector for the puzzle
-	string arg1( argv[ 1 ] ); // string containing the value of command line 
+    deque<tile> puzzleVec; // vector for the puzzle
+    string arg1( argv[ 1 ] ); // string containing the value of command line
 
-	// add all the elements to the vector
-	addElements( puzzleVec, stoi( arg1 ) );
+    // add all the elements to the vector
+    addElements( puzzleVec, stoi( arg1 ) );
 
-	print( puzzleVec );
+    print( puzzleVec );
 }
 
-void print( vector<int>& vec ) {
-	int rowIndex = 0; 
-	for ( int v : vec ) { // loop thru vector
+void addElements( deque<tile>& test, int puzzle ) {
 
-		if ( rowIndex < 3 ) { // only 3 items per row
-			cout << v << " ";
-			rowIndex++;
-		}
-		else { // 3 items met reset and go to next row
-			rowIndex = 1;
-			cout << endl << v << " ";
-		}
-	}
+    int row = 3;
+    int column = 3;
+    tile tempTile(0, 0, 0);
+    
+    // as long as input still has something
+    while ( puzzle > 0 ) {
+        // set the atributes of this tile note: int % 10 gets the ending digit
+        tempTile = tile(column, row, puzzle % 10);
+        
+        // push last tile onto stack
+        test.emplace_front( tempTile );
+        
+        // remove last tile
+        puzzle /= 10;
+        
+        // gotta do it in reverse since reading from the back
+        column--;
+        if (column < 1) {
+            row--;
+            column = 3;
+        }
+    }
 }
 
-void addElements( vector<int>& test, int puzzle ) {
+void print( deque<tile>& vec ) {
+    int rowIndex = 0;
+    for ( tile v : vec ) { // loop thru vector
 
-	// as long as input still has something
-	stack<int> st;
-	while ( puzzle > 0 ) {
-		st.push( puzzle % 10 ); // push last digit onto stack
-		puzzle /= 10; // remove last digit
-	}
-
-	// run thru the stack in reverse to put our list in correct order
-	// ex. 1234 currently is 4321 in the stack so push the top to back of vector to un-reverse
-	while ( !st.empty( ) ) {
-		test.push_back( st.top( ) );
-		st.pop( );
-	}
+        if ( rowIndex < 3 ) { // only 3 items per row
+            cout << "value: " << v.curValue
+            << " row: " << v.curRow
+            << " column: " << v.curCol << " ";
+            rowIndex++;
+        }
+        else { // 3 items met reset and go to next row
+            rowIndex = 1;
+            cout << endl << "value: " << v.curValue
+            << " row: " << v.curRow
+            << " column: " << v.curCol << " ";
+        }
+    }
+    
+    cout << endl;
 }
+
+
+/* valid moves *//*
+
+ validMoves(const board testBoard) {
+    // where is the blank (0)
+    int blankRow = 0;
+    int blankCol = 0;
+    
+    // if this move
+ }
+ */
